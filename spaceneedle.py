9f3ca0066ad2c5589a3c39facdfcc18aedd87046
Phil Boothe <pfb3cn@virginia.edu>
2016-09-02 08:40:48 -0400

    updated range functions

--- spaceneedle.py
+++ spaceneedle.py
@@ -7,65 +7,65 @@ By: Phil Boothe
 
 
 #thin needle - 4 rows
-for i in range(0,4):
+for i in range(4):
 	row = ''
-	for j in range(0,12):
+	for j in range(12):
 		row += ' '
 	row += '||'
 	print row
 #top of dome - 4 rows
-for i in range(0,4):
+for i in range(4):
 	row = ''
-	for j in range(0,9 - 3*i):
+	for j in range(9 - 3*i):
 		row += ' '
 	row += '__/'
-	for j in range(0, 3*i):
+	for j in range(3*i):
 		row += ':'
 	row += '||'
-	for j in range(0,3*i):
+	for j in range(3*i):
 		row += ':'
 	print row + '\\__'
 #center of dome
 row = '|'
-for j in range(0,24):
+for j in range(24):
 	row += '"'
 print row + '|'
 #bottom of dome - 4 rows
-for i in range(0,4):
+for i in range(4):
 	row = ''
-	for j in range(0, 0 + 2*i):
+	for j in range(0 + 2*i):
 		row += ' '
 	row += '\\_'
-	for j in range(0,11 - 2*i):
+	for j in range(11 - 2*i):
 		row += '/\\'
 	print row + '_/'
 #thin needle - 4 rows
-for i in range(0,4):
+for i in range(4):
 	row = ''
-	for j in range(0,12):
+	for j in range(12):
 		row += ' '
 	row += '||'
 	print row
 #thick needle - 16 rows
-for i in range(0,16):
+for i in range(16):
 	row = ''
-	for j in range(0,9):
+	for j in range(9):
 		row += ' '
 	print row + '|%%||%%|'
 #base - 4 rows
-for i in range(0,4):
+for i in range(4):
 	row = ''
-	for j in range(0,9 - 3*i):
+	for j in range(9 - 3*i):
 		row += ' '
 	row += '__/'
-	for j in range(0, 3*i):
+	for j in range(3*i):
 		row += ':'
 	row += '||'
-	for j in range(0,3*i):
+	for j in range(3*i):
 		row += ':'
 	print row + '\\__'
 #bottom row
 row = '|'
-for j in range(0,24):
+for j in range(24):
 	row += '"'
 print row + '|'
